6bbce5a161dd91a90cb055403692ae3dbdb03197
YahyaOmari <yahya.omari@outlook.com>
2021-05-10 14:53:36 +0300

    garage

--- tests/test_band.py
+++ tests/test_band.py
@@ -1,8 +1,8 @@
-# import json
-# import pytest
+import json
+import pytest
 # import yaml
 
-from pythonic_garage_band.band import Guitarist , Musician,Band , Bassist, Drummer
+from pythonic_garage_band.band import Guitarist , Musician,Band , Bassist, Drummer#, Musician
 
 def test_guitarist_str():
     joan = Guitarist("Joan Jett")
